20a6a39cf8d7957797be77d034c54a84f908f479
ColonelBHB <157838802+ColonelBHB@users.noreply.github.com>
2024-01-26 14:37:03 -0600

    Add files via upload

---
+++ Tail-Opacity/Tail-Opacity/Tail-Opacity/Tail_OpacityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tail_OpacityApp.swift
+//  Tail-Opacity
+//
+//  Created by Patrick Vogel on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Tail_OpacityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
